7ffa03ed681fc0cb88a0e8bca3b402081eaa5f87
myangaa1 <m.myanganbayar@gmail.com>
2021-09-13 16:39:21 +0800

    4

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 console.log('xaxaxa')
 
 
-axios('http://localhost:8888/api/v1/protocols/kp2_ai1')
+axios('https://tmservice.erdenetmc.mn/api/v1/protocols/kp2_ai1')
     .then(res => console.log(res.data))
     .catch(err => console.log(err.message))
 
