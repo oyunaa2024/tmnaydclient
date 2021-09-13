ac4007f478faf1a62eeffca458a09b9711815126
myangaa1 <m.myanganbayar@gmail.com>
2021-09-13 16:29:08 +0800

    4

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ axios('http://localhost:8888/api/v1/protocols/kp2_ai1')
     .then(res => console.log(res.data))
     .catch(err => console.log(err.message))
 
-axios('https://nayd.erdenetmc.mn/service/rmq/bind.php?id=rIYPDjeVEC0L94iJQz7e&tags%5B%5D=ELEC_KP7_AI3&tags%5B%5D=ELEC_KP2_DI64')
+axios('https://nayd.erdenetmc.mn/service/redis/get.php?tags%5B%5D=ELEC_KP7_AI3&tags%5B%5D=ELEC_KP2_DI64')
     .then(res => console.log(res))
     .catch(err => console.log(err.message))
 
