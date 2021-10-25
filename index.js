7b2923a2fb8b5eb7eca0ef52c55b81c469c6f9f5
myangaa1 <m.myanganbayar@gmail.com>
2021-10-25 16:47:23 +0800

    sdsd

--- index.js
+++ index.js
@@ -11,82 +11,89 @@ const db_scada = require("./config/db-mssql/scada");
 
 let id = makeID(20), tagToID = {}, values = {}, sum = {}, count = 0
 
+// const tmserviceHost = "https://tmservice.erdenetmc.mn"
+const tmserviceHost = "http://localhost:8888"
+
 async function init() {
-
-    for (let i = 0; i < ids.length; i++) {
-        let res = await axios(`http://localhost:8888/api/v1/signals/id/${ids[i]}`)
-        const tag = res.data.data.tagName.trim()
-        if (tag) {
-            tagToID[tag] = ids[i]
-            console.log(tag + '-->' + tagToID[tag])
-
-            res = await axios(`https://nayd.erdenetmc.mn/service/redis/get.php?tags[]=ELEC_${tag}`)
-            console.log(res.data)
-            if (res.data[0]) {
-                values[ids[i]] = JSON.parse(res.data[0]).d
-                sum[ids[i]] = 0
-                console.log(ids[i] + ' ==> ' + values[ids[i]])
+    try {
+        for (let i = 0; i < ids.length; i++) {
+            let res = await axios(`${tmserviceHost}/api/v1/signals/id/${ids[i]}`)
+            const tag = res.data.data.tagName.trim()
+            if (tag) {
+                tagToID[tag] = ids[i]
+                console.log(tag + '-->' + tagToID[tag])
+    
+                res = await axios(`https://nayd.erdenetmc.mn/service/redis/get.php?tags[]=ELEC_${tag}`)
+                console.log(res.data)
+                if (res.data[0]) {
+                    values[ids[i]] = JSON.parse(res.data[0]).d
+                    sum[ids[i]] = 0
+                    console.log(ids[i] + ' ==> ' + values[ids[i]])
+                }
+                else {
+                    values[ids[i]] = 0
+                    console.log(`False tag is '${tag}'' res.data[0] ==> ${res.data[0]}`)
+                }
+                    
             }
-            else {
-                values[ids[i]] = 0
-                console.log(`False tag is '${tag}'' res.data[0] ==> ${res.data[0]}`)
-            }
-                
         }
-    }
-
-    const on_connect = () => {
-
-        console.warn('WebSocket connected');
-
-        for (var tag in tagToID) {
-            axios.get(`https://nayd.erdenetmc.mn/service/rmq/bind.php?id=${id}&tags[]=ELEC_${tag}`)
-                .then(data => {
-                    client.subscribe("/amq/queue/temp_" + id, message => {
-                        const json = JSON.parse(message.body)
-                        json.tag = message.headers.destination.substring(message.headers.destination.lastIndexOf("/") + 1)
-                        json.tag = json.tag.substring(5, json.tag.length).split('.').join('_')
-                        // console.log(json)
-                        values[tagToID[json.tag]] = parseFloat(json.d)
+    
+        const on_connect = () => {
+    
+            console.warn('WebSocket connected');
+    
+            for (var tag in tagToID) {
+                axios.get(`https://nayd.erdenetmc.mn/service/rmq/bind.php?id=${id}&tags[]=ELEC_${tag}`)
+                    .then(data => {
+                        client.subscribe("/amq/queue/temp_" + id, message => {
+                            const json = JSON.parse(message.body)
+                            json.tag = message.headers.destination.substring(message.headers.destination.lastIndexOf("/") + 1)
+                            json.tag = json.tag.substring(5, json.tag.length).split('.').join('_')
+                            // console.log(json)
+                            values[tagToID[json.tag]] = parseFloat(json.d)
+                        })
                     })
-                })
+                    .catch(err => console.log(err.message))
+            }
+        }
+        const on_error = error => {
+            console.warn('WebSocket connection closed for Error');
+            console.log("on_error функц алдаа илгээлээ: ", error)
+        }
+    
+        const client = Stomp.overWS('wss://rabbit.erdenetmc.mn:15673/ws')
+        client.connect(headers, on_connect, on_error)
+    
+        setInterval(() => {
+            let now = dayjs()
+            for(let key in values)
+                sum[key] += values[key]
+    
+            count++
+    
+            if(new Date().getSeconds() == 0) {
+    
+                for(let key in values)
+                  sum[key] = sum[key] / count
+    
+                  console.log('Inserted to sql ==>', now.format('YYYY-MM-DD HH:mm:ss'))
+                  console.log(`Count ==> ${count}`)
+    
+                db_scada.Last_24Hour_AI_Graphic_m
+                .create({ ValueDate: now.format('YYYY-MM-DD HH:mm:ss'), ...sum })
+                .then(r => console.log('Inserted to sql ==>', now.format('YYYY-MM-DD HH:mm:ss')))
                 .catch(err => console.log(err.message))
-        }
+    
+                for(let key in values)
+                  sum[key] = 0
+    
+                count = 0
+            }
+        }, 1000)
     }
-    const on_error = error => {
-        console.warn('WebSocket connection closed for Error');
-        console.log(error)
+    catch(err) {
+        console.log("Init функц дотроос алдаа гарлаа: ", err)
     }
-
-    const client = Stomp.overWS('wss://rabbit.erdenetmc.mn:15673/ws')
-    client.connect(headers, on_connect, on_error)
-
-    setInterval(() => {
-        let now = dayjs()
-        for(let key in values)
-            sum[key] += values[key]
-
-        count++
-
-        if(new Date().getSeconds() == 0) {
-
-            for(let key in values)
-              sum[key] = sum[key] / count
-
-              console.log('Inserted to sql ==>', now.format('YYYY-MM-DD HH:mm:ss'))
-              console.log(`Count ==> ${count}`)
-
-            db_scada.Last_24Hour_AI_Graphic_m
-            .create({ ValueDate: now.format('YYYY-MM-DD HH:mm:ss'), ...sum })
-            .then(r => console.log('Inserted to sql ==>', now.format('YYYY-MM-DD HH:mm:ss')))
-            .catch(err => console.log(err.message))
-
-            for(let key in values)
-              sum[key] = 0
-
-            count = 0
-        }
-    }, 1000)
 }
 
 init()
