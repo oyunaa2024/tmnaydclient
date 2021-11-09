3f936a3411e17d93546539c0eb2caa8953a5d946
myangaa1 <m.myanganbayar@gmail.com>
2021-11-09 12:51:02 +0800

    newupdate

--- tag_test.js
+++ tag_test.js
@@ -9,33 +9,34 @@ dotenv.config({ path: "./config/config.env" });
 const db_scada = require("./config/db-mssql/scada");
 
 const id = makeID(20)
-const values = {}, sum = {}
-let count = 0
+const analogTags = {}
 
-// const tmserviceHost = "https://tmservice.erdenetmc.mn"
-const tmserviceHost = "http://localhost:8888"
 
 async function init() {
     try {
         for (let i = 0; i < tags.length; i++) {
                
+            res = await axios(`https://nayd.erdenetmc.mn/service/redis/get.php?tags[]=ELEC_${tags[i]}`)
+
+            if (res.data[0]) {
                 console.log(`Таг : ${tags[i]}`)
-
-                res = await axios(`https://nayd.erdenetmc.mn/service/redis/get.php?tags[]=ELEC_${tags[i]}`)
-
-                if (res.data[0]) {
-                    values[tags[i]] = JSON.parse(res.data[0]).d
-                    console.log(`Анхны утга : ${values[tags[i]]}`)
-                    console.log("------------")
-                    sum[tags[i]] = 0
+                console.log(`Анхны утга : ${JSON.parse(res.data[0]).d}`)
+                console.log("------------")
+                analogTags[tags[i]] = {
+                    total: JSON.parse(res.data[0]).d,
+                    count: 1
                 }
-                else {
-                    values[tags[i]] = 0
-                    console.log("+++++++++++++++++++++++++++++WARNING++++++++++++++++++++++++++++++++++++++")
-                    console.log(`False tag[i] is '${tags[i]}'' res.data[0] ==> ${res.data[0]}`)
-                    console.log("++++++++++++++++++++++++++++WARNING END+++++++++++++++++++++++++++++++++++")
+            }
+            else {
+                console.log("+++++++++++++++++++++++++++++WARNING++++++++++++++++++++++++++++++++++++++")
+                console.log(`False tag[i] is '${tags[i]}'' res.data[0] ==> ${res.data[0]}`)
+                console.log("++++++++++++++++++++++++++++WARNING END+++++++++++++++++++++++++++++++++++")
+                analogTags[tags[i]] = {
+                    total: 0,
+                    count: 0
                 }
-                console.log("--------------------------------------------------------------------------------")
+            }
+
         }
     
         const on_connect = async () => {
@@ -44,56 +45,66 @@ async function init() {
     
             for (let i = 0; i < tags.length; i++) {
                 console.log(`Connection RANDOM ID << ${id} >>`)
-
                 await axios.get(`https://nayd.erdenetmc.mn/service/rmq/bind.php?id=${id}&tags[]=ELEC_${tags[i]}`)
                 console.log(`Bind success => ${tags[i]}`)
             }
 
             client.subscribe("/amq/queue/temp_" + id, message => {
-                const json = JSON.parse(message.body)
-                json.tag = message.headers.destination.substring(message.headers.destination.lastIndexOf("/") + 1)
-                json.tag = json.tag.substring(5, json.tag.length).split('.').join('_')
-                values[json.tag] = json.d
+                const json = JSON.parse(message.body);
+                json.tag = message.headers.destination.substring(message.headers.destination.lastIndexOf("/") + 1);
+                json.tag = json.tag.substring(5, json.tag.length).split('.').join('_');
+
                 // console.log(json)
+                analogTags[json.tag].total += json.d;
+                analogTags[json.tag].count += 1;
+                // console.log(analogTags)
+               
             })
         }
         const on_error = error => {
             console.log('WebSocket холболт салсан ==> Error :', error);
+            clearInterval(mainInterval)
         }
     
         const client = Stomp.overWS('wss://rabbit.erdenetmc.mn:15673/ws')
         client.connect(headers, on_connect, on_error)
 
-       
 
-        setInterval(() => {
-          
-            tags.forEach(tag => sum[tag] += values[tag]);
-            count++;
+        const myTimer = setInterval(() => {
+            console.log("++ My Timer Worked ++")
 
             if(new Date().getSeconds() == 0) {
-
-                const avarage = {};
-
-                tags.forEach(tag => {
-                   avarage[tag] = sum[tag] / count;
-                   sum[tag] = 0;
-                });
-
-                count = 0;
-
                 let now = dayjs();
-                // console.log(`"${now.format('YYYY-MM-DD HH:mm:ss')}" 1 минутын дундаж SQL серверлүү бичигдлээ`)
-                // console.log(avarage)
-                 db_scada.Last_24Hour_AI_Graphic_m1
-                    .create({ ValueDate: now.format('YYYY-MM-DD HH:mm:ss'), ...avarage })
-                    .then(r => console.log(`"${now.format('YYYY-MM-DD HH:mm:ss')}" 1 минутын дундаж SQL серверлүү бичигдлээ`))
-                    .catch(err => console.log(err.response ? err.response.data : err.message))
+                console.log("<<<<<<<<<<< Main Timer Started >>>>>>>>>>>>")
+                console.log(now.format('YYYY-MM-DD HH:mm:ss'))
+
+                const mainInterval = setInterval(() => {
+                    const average = {};
+
+                    for (const [key, value] of Object.entries(analogTags)) {
+                        average[key] = value.total / value.count;
+                        value.total = 0;
+                        value.count = 0;
+                    }
+
+                    let now = dayjs();
+                   
+                    db_scada.Last_24Hour_AI_Graphic_m1
+                        .create({ ValueDate: now.format('YYYY-MM-DD HH:mm:ss'), ...average })
+                        .then(r => console.log(`"${now.format('YYYY-MM-DD HH:mm:ss')}" 1 минутын дундаж SQL серверлүү бичигдлээ`))
+                        .catch(err => console.log(err.response ? err.response.data : err.message))
+
+                }, 60 * 1000)
+
+                console.log("MyTimerStop")
+                clearInterval(myTimer)
             }
+
         }, 1000)
     }
     catch(err) {
         console.log("Init функц дотроос алдаа гарлаа: ", err.response ? err.response.data : err.message)
+        clearInterval(mainInterval)
     }
 }
 
--- utils/base.js
+++ utils/base.js
@@ -1115,6 +1115,6 @@ exports.tags = [
 "KP10_AI7"
 ]
 exports.tags1 = [
-"KP2_AI1",
-"KP2_AI2"
+"KP3_PII_GOKB_AI1",
+"KP3_PII_GOKA_AI1"
 ]
