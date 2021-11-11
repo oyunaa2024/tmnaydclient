9fcf6a6a74f00a04a5d73264b8b5f06d1bced860
myangaa1 <m.myanganbayar@gmail.com>
2021-11-11 16:13:10 +0800

    finish

--- tag_test.js
+++ tag_test.js
@@ -2,16 +2,16 @@ const axios = require('axios')
 const Stomp = require('stompjs')
 const dayjs = require('dayjs')
 const makeID = require('./utils/makeID')
-const {ids, headers, tags } = require('./utils/base')
+const {headers, tags } = require('./utils/base')
 const dotenv = require('dotenv')
 dotenv.config({ path: "./config/config.env" });
 
 const db_scada = require("./config/db-mssql/scada");
 
 const id = makeID(20)
-let analogTags = {}
-let resetTags = {}
-
+let analogTagsRealValues = {};
+let mainCount = 0;
+let sum = {};
 
 async function init() {
     try {
@@ -23,27 +23,15 @@ async function init() {
                 console.log(`Таг : ${tags[i]}`)
                 console.log(`Анхны утга : ${JSON.parse(res.data[0]).d}`)
                 console.log("------------")
-                analogTags[tags[i]] = {
-                    total: JSON.parse(res.data[0]).d,
-                    count: 1
-                }
-                resetTags[tags[i]] = {
-                    total: 0,
-                    count: 0
-                }
+                analogTagsRealValues[tags[i]] = JSON.parse(res.data[0]).d;
+                sum[tags[i]] = 0;
             }
             else {
                 console.log("+++++++++++++++++++++++++++++WARNING++++++++++++++++++++++++++++++++++++++")
                 console.log(`False tag[i] is '${tags[i]}'' res.data[0] ==> ${res.data[0]}`)
                 console.log("++++++++++++++++++++++++++++WARNING END+++++++++++++++++++++++++++++++++++")
-                analogTags[tags[i]] = {
-                    total: 0,
-                    count: 1
-                }
-                resetTags[tags[i]] = {
-                    total: 0,
-                    count: 0
-                }
+                analogTagsRealValues[tags[i]] = 0;
+                sum[tags[i]] = 0;
             }
 
         }
@@ -63,68 +51,55 @@ async function init() {
                 json.tag = message.headers.destination.substring(message.headers.destination.lastIndexOf("/") + 1);
                 json.tag = json.tag.substring(5, json.tag.length).split('.').join('_');
 
-                // console.log(json)
-                analogTags[json.tag].total += json.d;
-                analogTags[json.tag].count += 1;
-                // console.log(analogTags)
-               
+                analogTagsRealValues[json.tag] = json.d;
             })
         }
         const on_error = error => {
             console.log('WebSocket холболт салсан ==> Error :', error);
-            clearInterval(mainInterval)
+            clearInterval(mainTimer)
         }
     
         const client = Stomp.overWS('wss://rabbit.erdenetmc.mn:15673/ws')
         client.connect(headers, on_connect, on_error)
 
 
-        const myTimer = setInterval(() => {
-            console.log("++ My Timer Worked ++")
+        const mainTimer = setInterval(() => {
 
+            const analogTagsRealValuesCopy = {...analogTagsRealValues};
+
+            for (const [key, value] of Object.entries(analogTagsRealValuesCopy)) {
+                sum[key] += value;
+            };
+
+            mainCount += 1;
+         
             if(new Date().getSeconds() == 0) {
-                let now = dayjs();
-                console.log("<<<<<<<<<<< Main Timer Started >>>>>>>>>>>>")
-                console.log(now.format('YYYY-MM-DD HH:mm:ss'))
 
-                const mainInterval = setInterval(() => {
+                const average = {};
 
-                    const analogTagsCopy = {...analogTags};
-                    const average = {};
+                for (const [key, value] of Object.entries(sum)) {
+                    average[key] = value / mainCount;
+                    sum[key] = 0;           
+                }
 
-                    for (const [key, value] of Object.entries(analogTagsCopy)) {
+                mainCount = 0;
 
-                        if(value.count === 0){
-                            average[key] = value.total;
-                        }
-                        else {
-                            average[key] = value.total / value.count;
-                            resetTags[key].total = value.total / value.count;
-                            resetTags[key].count = 1;
-                        }
-                        
-                    }
+                // SQL серверлүү бичих хэсэг
+                db_scada.Last_24Hour_AI_Graphic_m1
+                    .create({ ValueDate: now.format('YYYY-MM-DD HH:mm:ss'), ...average })
+                    .then(r => {
+                        let now = dayjs()
+                        console.log(`"${now.format('YYYY-MM-DD HH:mm:ss')}" 1 минутын дундаж SQL серверлүү бичигдлээ`)
+                    })
+                    .catch(err => console.log(err.response ? err.response.data : err.message))
 
-                    let now = dayjs();
-               
-                    db_scada.Last_24Hour_AI_Graphic_m1
-                        .create({ ValueDate: now.format('YYYY-MM-DD HH:mm:ss'), ...average })
-                        .then(r => console.log(`"${now.format('YYYY-MM-DD HH:mm:ss')}" 1 минутын дундаж SQL серверлүү бичигдлээ`))
-                        .catch(err => console.log(err.response ? err.response.data : err.message))
-                    
-                    analogTags = {...resetTags};
-
-                }, 60 * 1000)
-
-                console.log("My Timer Stoped !!!")
-                clearInterval(myTimer)
             }
 
-        }, 1000)
+        }, 1000);
     }
     catch(err) {
         console.log("Init функц дотроос алдаа гарлаа: ", err.response ? err.response.data : err.message)
-        clearInterval(mainInterval)
+        clearInterval(mainTimer)
     }
 }
 
