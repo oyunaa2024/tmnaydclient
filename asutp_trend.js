bdffbb4e81c8fdd5e8e94cb3ff5e6a5fd5c41989
myangaa1 <m.myanganbayar@gmail.com>
2022-09-05 16:07:59 +0800

    asu

--- asutp_trend.js
+++ asutp_trend.js
@@ -10,118 +10,118 @@ const db_signal_asutp = require("./config/db-mssql/signal_asutp");
 
 
 const asutpAnalogSignalIds = {
-    // 700031:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700014:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700048:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700065:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700082:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700099:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700116:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700133:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700150:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700019:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700001:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700036:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700053:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700070:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700087:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700104:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700121:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 700138:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 300401:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 300343:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 300345:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 300344:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 300346:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 300410:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 300411:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 300425:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 300426:{
-    //     values:[],
-    //     lastValue: "",
-    // },
-    // 300332:{
-    //     values:[],
-    //     lastValue: "",
-    // },
+    700031:{
+        values:[],
+        lastValue: "",
+    },
+    700014:{
+        values:[],
+        lastValue: "",
+    },
+    700048:{
+        values:[],
+        lastValue: "",
+    },
+    700065:{
+        values:[],
+        lastValue: "",
+    },
+    700082:{
+        values:[],
+        lastValue: "",
+    },
+    700099:{
+        values:[],
+        lastValue: "",
+    },
+    700116:{
+        values:[],
+        lastValue: "",
+    },
+    700133:{
+        values:[],
+        lastValue: "",
+    },
+    700150:{
+        values:[],
+        lastValue: "",
+    },
+    700019:{
+        values:[],
+        lastValue: "",
+    },
+    700001:{
+        values:[],
+        lastValue: "",
+    },
+    700036:{
+        values:[],
+        lastValue: "",
+    },
+    700053:{
+        values:[],
+        lastValue: "",
+    },
+    700070:{
+        values:[],
+        lastValue: "",
+    },
+    700087:{
+        values:[],
+        lastValue: "",
+    },
+    700104:{
+        values:[],
+        lastValue: "",
+    },
+    700121:{
+        values:[],
+        lastValue: "",
+    },
+    700138:{
+        values:[],
+        lastValue: "",
+    },
+    300401:{
+        values:[],
+        lastValue: "",
+    },
+    300343:{
+        values:[],
+        lastValue: "",
+    },
+    300345:{
+        values:[],
+        lastValue: "",
+    },
+    300344:{
+        values:[],
+        lastValue: "",
+    },
+    300346:{
+        values:[],
+        lastValue: "",
+    },
+    300410:{
+        values:[],
+        lastValue: "",
+    },
+    300411:{
+        values:[],
+        lastValue: "",
+    },
+    300425:{
+        values:[],
+        lastValue: "",
+    },
+    300426:{
+        values:[],
+        lastValue: "",
+    },
+    300332:{
+        values:[],
+        lastValue: "",
+    },
     300334:{
         values:[],
         lastValue: "",
@@ -131,9 +131,9 @@ const asutpAnalogSignalIds = {
 // 300333 300334
 
 
-// const insertTimer = setInterval(() => {
+const insertTimer = setInterval(() => {
     insertToSql();
-// }, 60 * 1000);
+}, 60 * 1000);
 
 async function insertToSql() {
     const data = [];
